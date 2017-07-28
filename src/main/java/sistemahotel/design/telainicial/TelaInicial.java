0622b866ee3edb778b886072bbb862eecdc1e092
marceloyb <marcelobenesciutti@gmail.com>
2017-07-28 19:19:56 -0300

    AI CARAI

--- src/main/java/sistemahotel/design/telainicial/TelaInicial.java
+++ src/main/java/sistemahotel/design/telainicial/TelaInicial.java
@@ -39,7 +39,7 @@ public class TelaInicial{
     public void btClientesActionHandler(ActionEvent e) {
         Stage stage = new Stage();
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/fxml/fxml_clientes/gerenciador_cliente"));
+        loader.setLocation(getClass().getResource("/fxml/fxml_clientes/gerenciador_cliente.fxml"));
         Parent root = null;
         try {
             root = loader.load();
